a81056faf10157f81e520f9becfa724cf4611b82
Vrusha Shah <vrushashah17@gmail.com>
2022-07-20 23:24:14 +0000

    Updates the names of panic css file to avoid conflicts

---
+++ target/classes/META-INF/resources/Cookies.js
@@ -0,0 +1,34 @@
+// Module to Handle Cookies
+
+export function storeLoginSession(username,passwordHash){
+    // Store username and password hash as cookies.
+    setCookie("username",username,2);
+    setCookie("passwordHash",passwordHash,2);
+}
+
+// Set a Cookie
+export function setCookie(cName, cValue, expDays) {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+
+// Get a Cookie with a given name
+export function getCookieValue(name){
+    const value = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || '';
+    return value;
+}
+
+export function getCurrentUsername(){
+    return getCookieValue("username");
+}
+
+export function getCurrentPasswordHash(){
+    return getCookieValue("passwordHash");
+}
+
+// Delete a cookie
+export function deleteCookie(cName) {
+    document.cookie = cName + "=; Path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+}
---
+++ target/classes/META-INF/resources/debugger.js
@@ -0,0 +1,123 @@
+import {getUser,getUserCalendar,getUserJournal,getUserPanicButton,addDayToCalendar,addEntryToJournal,addOneToPanicButton,deleteCurrentUser,queryJournalEntries} from "../api/Datastore-API.js";
+import {getCurrentUsername,getCurrentPasswordHash,deleteCookie} from "./Cookies.js"
+import {updateText} from "./updateText.js";
+import {redirectToLoginIfNotLoggedIn} from "./check_if_logged_in.js";
+
+window.onLoad = async function(){
+
+    redirectToLoginIfNotLoggedIn();
+
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    updateText("Hello, " + currentUsername + "!","greeter");
+
+    updateText(JSON.stringify(await getUserCalendar(currentUsername,currentPasswordHash)),"my-calendar");
+
+    updateText(JSON.stringify(await getUserJournal(currentUsername,currentPasswordHash)),"my-journal");
+
+    updateText(await getUserPanicButton(currentUsername,currentPasswordHash),"my-panic-button");
+
+    updateText(JSON.stringify(await getUser(currentUsername,currentPasswordHash)),"my-info");
+};
+
+window.addToCalendar = async function addToCalendar(){
+
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    console.log("Data:",currentUsername,currentPasswordHash);
+
+    const date = document.getElementById("day-date").value;
+
+    const mood = document.getElementById("day-mood").value;
+
+    console.log("Input day:",date,mood);
+
+    const response = await addDayToCalendar(currentUsername, currentPasswordHash, date, mood);
+
+    console.log(await response);
+    if (await response.error) {
+        alert("addToCalendar failed: " + response.error);
+    }
+    else{
+        updateText(JSON.stringify(await getUserCalendar(currentUsername,currentPasswordHash)),"my-calendar");
+        updateText(JSON.stringify(await getUser(currentUsername,currentPasswordHash)),"my-info");
+    }
+
+};
+
+window.addToJournal = async function addToJournal(){
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    const date = document.getElementById("entry-date").value;
+    const entry = document.getElementById("journal-entry").value;
+
+    const response = await addEntryToJournal(currentUsername, currentPasswordHash, entry, date);
+
+    console.log(await response);
+    if (await response.error) {
+        alert("addToJournal failed: " + response.error);
+    }
+    else{
+        updateText(JSON.stringify(await getUserJournal(currentUsername,currentPasswordHash)),"my-journal");
+        updateText(JSON.stringify(await getUser(currentUsername,currentPasswordHash)),"my-info");
+        
+    }
+};
+
+window.updatePanicButton = async function updatePanicButton(){
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    const response = await addOneToPanicButton(currentUsername, currentPasswordHash);
+
+    console.log(await response);
+    if (await response.error) {
+        alert("addOneToPanicButton failed: " + response.error);
+    }
+    else{
+        updateText(await getUserPanicButton(currentUsername,currentPasswordHash),"my-panic-button");
+    }
+};
+
+window.logout = function logout(){
+    deleteCookie("username");
+    deleteCookie("passwordHash");
+    window.location.href = "/login.html";    
+};
+
+window.userDelete = async function userDelete(){
+
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    const response = await deleteCurrentUser(currentUsername,currentPasswordHash);
+
+    console.log(await response);
+    if (await response.error) {
+        alert("delete failed: " + response.error);
+    }
+    else{
+        alert(response.success);
+        deleteCookie("username");
+        deleteCookie("passwordHash");
+        window.location.href = "/login.html";    
+    }
+};
+
+window.queryJournal = async function queryJournal(){
+
+    const currentUsername = getCurrentUsername();
+    const currentPasswordHash = getCurrentPasswordHash();
+
+    const startingDate = document.getElementById("starting-date").value;
+    const endingDate = document.getElementById("ending-date").value;
+
+    const response = await queryJournalEntries(currentUsername, currentPasswordHash, startingDate, endingDate)
+
+    updateText(JSON.stringify(await response),"my-query")
+
+
+};
