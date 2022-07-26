a098edda1ff59922efb9cd559c484df7eabadbb7
GuillermoTafoya <tafoyamilo@gmail.com>
2022-07-26 02:31:04 +0000

    Panic button counter works.

--- src/main/webapp/homepage/homepage.js
+++ src/main/webapp/homepage/homepage.js
@@ -1,7 +1,8 @@
-import {getCurrentUsername} from "../Cookies.js"
+import {getCurrentUsername,getCurrentPasswordHash} from "../Cookies.js"
 import {updateText} from "../updateText.js";
 import {redirectToLoginIfNotLoggedIn} from "../../check_if_logged_in.js"
 import {getQuote} from "./components/quote.js"
+import {addOneToPanicButton} from "../api/Datastore-API.js"
 
 //loads the user's name to add it as a greeter
 window.onLoad = async function(){
@@ -26,4 +27,14 @@ window.openPopupTwo = function openPopupTwo(){
 
 window.closePopupTwo = function closePopupTwo(){
     popup2.classList.remove("open-popup");
+}
+
+window.handlePopUpMaps = async function(){
+    await addOneToPanicButton(getCurrentUsername(), getCurrentPasswordHash());
+    window.location.href ='/maps/maps.html';
+}
+
+window.handlePopUpChat = async function(){
+    await addOneToPanicButton(getCurrentUsername(), getCurrentPasswordHash());
+    window.location.href = '/chat/chat.html';
 }
